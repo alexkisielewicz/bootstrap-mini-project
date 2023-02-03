4e42c186f9aa71e3093c4afdd1267419746ab239
alexkisielewicz <aleksander.kisielewicz@gmail.com>
2023-02-03 20:05:36 +0000

    Implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    let templateParams = {
+				"from_name": contactForm.name.value,
+				"from_email": contactForm.emailaddress.value,
+                "project_request": contactForm.projectsummary.value
+			};
+
+	let publicKey = "g1axgIaSxxc6CzUiB";
+			
+    emailjs.init("service_uds44db");
+    emailjs.send("service_uds44db", "bootstrap", templateParams, publicKey)
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            }, 
+            function(error) {
+                console.log("FAILED", error);
+            });
+} 
